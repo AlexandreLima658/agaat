792443d759c71321da8841ff1461c8e9c0b4d8b8
Carlos Alexandre <alexandrec@alu.ufc.br>
2024-02-06 10:45:57 -0300

    test: cobertura de teste da criacao de cecanes na camada de aplicacao

--- application/src/test/java/br/gov/pnae/agaat/application/cecanes/create/CreateCecaneUseCaseTest.java
+++ application/src/test/java/br/gov/pnae/agaat/application/cecanes/create/CreateCecaneUseCaseTest.java
@@ -1,12 +1,28 @@
 package br.gov.pnae.agaat.application.cecanes.create;
 
+import br.gov.pnae.agaat.domain.cecanes.Cecane;
 import br.gov.pnae.agaat.domain.cecanes.CecaneRepository;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.*;
+
 @Tag("unitTest")
 class CreateCecaneUseCaseTest {
-    void shouldCreateCecaneUseCase(){
+    @Test
+    void givenCreateCecaneUseCase_whenCreateCecane_thenCecaneIsCreated() {
+       //given
+       CecaneRepository mockCecaneRepository = mock(CecaneRepository.class);
+       CreateCecaneUseCase createCecaneUseCase = new CreateCecaneUseCase(mockCecaneRepository);
+
+         Input input = new Input("Cecane 1");
+
+            Cecane cecane = input.toAggregate();
+            when(mockCecaneRepository.persist(cecane)).thenReturn(cecane);
+            //when
+            Output output = createCecaneUseCase.execute(input);
+            //then
+            verify(mockCecaneRepository, times(0)).persist(cecane);
 
     }
 }
