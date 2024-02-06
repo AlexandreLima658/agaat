a27818bad08d5773022d160b3d90000b27e9c382
Carlos Alexandre <alexandrec@alu.ufc.br>
2024-02-06 12:04:41 -0300

    test: cobertura de teste das Domainexceptions

---
+++ domain/src/test/java/br/gov/pnae/agaat/domain/commons/exceptions/DomainExceptionTest.java
@@ -0,0 +1,30 @@
+package br.gov.pnae.agaat.domain.commons.exceptions;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("unitTest")
+class DomainExceptionTest {
+    @Test
+    void shouldDomainException() {
+        //given
+        final String message = "Teste";
+        //when
+        final DomainException domainException = new DomainException(message);
+        //then
+        assert domainException.getMessage().equals(message);
+    }
+
+    @Test
+    void shouldDomainExceptionWithCause() {
+        //given
+        final String message = "Teste";
+        final Throwable cause = new Throwable();
+        //when
+        final DomainException domainException = new DomainException(message, cause);
+        //then
+        assert domainException.getMessage().equals(message);
+        assert domainException.getCause().equals(cause);
+    }
+
+}
